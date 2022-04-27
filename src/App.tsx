c3c933ff1c11922b7ff667bfbae9f4cfd6612653
Matozinho <matozinhof.academico@gmail.com>
2022-04-27 01:22:10 -0300

    feat(gt-ui): integrate sphere GTs with UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PlusOutlined } from '@ant-design/icons'
+import { PlusOutlined } from "@ant-design/icons";
 import {
   Button,
   Col,
@@ -13,35 +13,43 @@ import {
   Slider,
   Tooltip,
   Typography,
-} from 'antd'
-import { useState } from 'react'
-import { useSceneContext } from './contexts/Scene'
-import {
-  Sphere,
-  SphereConstructorType,
-  SphereType,
-} from './objects/Sphere'
-import { P5Interface } from './scketches/P5Interface'
-const { Sider, Content } = Layout
-const { Option } = Select
-const { Panel } = Collapse
+} from "antd";
+import { useState } from "react";
+import { useSceneContext } from "./contexts/Scene";
+import { Sphere, SphereConstructorType, SphereType } from "./objects/Sphere";
+import { P5Interface } from "./scketches/P5Interface";
+const { Sider, Content } = Layout;
+const { Option } = Select;
+const { Panel } = Collapse;
 
 function App() {
-  const [
-    createSphereModalVisible,
-    setCreateSphereModalVisible,
-  ] = useState(false)
+  const [createSphereModalVisible, setCreateSphereModalVisible] =
+    useState(false);
   const {
     sceneObjects,
     setSelectedSphereId,
-    selectedSphereId,
-  } = useSceneContext()
+    translateSelectedObject,
+    scaleSelectedObject,
+    rotateSelectedObject,
+  } = useSceneContext();
+
+  const [translateX, setTranslateX] = useState<number>(0);
+  const [translateY, setTranslateY] = useState<number>(0);
+  const [translateZ, setTranslateZ] = useState<number>(0);
+
+  const [scaleX, setScaleX] = useState<number>(1);
+  const [scaleY, setScaleY] = useState<number>(1);
+  const [scaleZ, setScaleZ] = useState<number>(1);
+
+  const [rotateX, setRotateX] = useState<number>(0);
+  const [rotateY, setRotateY] = useState<number>(0);
+  const [rotateZ, setRotateZ] = useState<number>(0);
 
   return (
     <Layout
       style={{
-        overflow: 'hidden',
-        height: '100vh',
+        overflow: "hidden",
+        height: "100vh",
       }}
       hasSider
     >
@@ -49,271 +57,279 @@ function App() {
         visible={createSphereModalVisible}
         setVisibility={setCreateSphereModalVisible}
       />
-      <Content id='mainCanvas'>
+      <Content id="mainCanvas">
         <P5Interface />
       </Content>
       <Sider
         style={{
-          overflow: 'auto',
-          position: 'sticky',
-          padding: '1rem',
+          overflow: "auto",
+          position: "sticky",
+          padding: "1rem",
           right: 0,
           top: 0,
         }}
       >
-        <Typography.Title
-          level={2}
-          style={{ color: 'white' }}
-        >
+        <Typography.Title level={2} style={{ color: "white" }}>
           Sphere Generator
         </Typography.Title>
         <Collapse bordered={false}>
-          <Panel key={1} showArrow={false} header='Spheres'>
+          <Panel key={1} showArrow={false} header="Spheres">
             <p>Select a Sphere:</p>
             <Select
               allowClear
-              style={{ width: '100%' }}
-              onChange={(value) =>
-                setSelectedSphereId(value)
-              }
+              style={{ width: "100%" }}
+              onChange={(value) => setSelectedSphereId(value)}
             >
               {sceneObjects.map((sphere: SphereType) => {
                 return (
                   <Option key={sphere.id} value={sphere.id}>
                     {sphere.name}
                   </Option>
-                )
+                );
               })}
             </Select>
           </Panel>
         </Collapse>
         <Typography.Title
           level={4}
-          style={{ color: 'white', margin: '1rem 0' }}
+          style={{ color: "white", margin: "1rem 0" }}
         >
           Geometric Transformations
         </Typography.Title>
         <Collapse
           bordered={false}
-          collapsible={
-            selectedSphereId ? 'header' : 'disabled'
-          }
+          // collapsible={selectedSphereId ? "header" : "disabled"}
         >
-          <Panel
-            key={1}
-            showArrow={false}
-            header='Rotation'
-          >
+          <Panel key={1} showArrow={false} header="Rotation">
             <label>X Rotation</label>
-            <Slider />
+            <Slider
+              min={0}
+              max={360}
+              step={0.5}
+              onChange={(value) => {
+                setRotateX(value);
+                rotateSelectedObject("x", value - rotateX);
+              }}
+            />
 
             <label>Y Rotation</label>
-            <Slider />
+            <Slider
+              min={0}
+              max={360}
+              step={0.5}
+              onChange={(value) => {
+                setRotateY(value);
+                rotateSelectedObject("y", value - rotateY);
+              }}
+            />
 
             <label>Z Rotation</label>
-            <Slider />
+            <Slider
+              min={0}
+              max={360}
+              step={0.5}
+              onChange={(value) => {
+                setRotateZ(value);
+                rotateSelectedObject("z", value - rotateZ);
+              }}
+            />
           </Panel>
-          <Panel key={2} showArrow={false} header='Scale'>
+          <Panel key={2} showArrow={false} header="Scale">
             <label>X Scale</label>
             <InputNumber
+              defaultValue={0}
+              onChange={(value) => setScaleX(value || 1)}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
 
             <label>Y Scale</label>
             <InputNumber
+              defaultValue={1}
+              onChange={(value) => setScaleY(value || 1)}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
 
             <label>Z Scale</label>
             <InputNumber
+              defaultValue={1}
+              onChange={(value) => setScaleZ(value || 1)}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
+            <div style={{ textAlign: "center" }}>
+              <Button
+                type="primary"
+                onClick={() => scaleSelectedObject(scaleX, scaleY, scaleZ)}
+              >
+                Scale
+              </Button>
+            </div>
           </Panel>
-          <Panel
-            key={3}
-            showArrow={false}
-            header='Translation'
-          >
+          <Panel key={3} showArrow={false} header="Translation">
             <label>X Translation</label>
             <InputNumber
+              onChange={(value: number) => setTranslateX(value || 0)}
+              defaultValue={0}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
 
             <label>Y Translation</label>
             <InputNumber
+              onChange={(value: number) => setTranslateY(value || 0)}
+              defaultValue={0}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
 
             <label>Z Translation</label>
             <InputNumber
+              onChange={(value: number) => setTranslateZ(value || 0)}
+              defaultValue={0}
               style={{
-                width: '100%',
-                marginBottom: '0.5rem',
+                width: "100%",
+                marginBottom: "0.5rem",
               }}
             />
+            <div style={{ textAlign: "center", marginTop: "1rem" }}>
+              <Button
+                type="primary"
+                onClick={() => {
+                  translateSelectedObject(translateX, translateY, translateZ);
+                }}
+              >
+                Translate
+              </Button>
+            </div>
           </Panel>
         </Collapse>
-        <Row justify='end'>
-          <Tooltip title='Create New Sphere'>
+        <Row justify="end">
+          <Tooltip title="Create New Sphere">
             <Button
               style={{
-                margin: '1rem 0',
+                margin: "1rem 0",
               }}
-              type='primary'
-              size='large'
-              shape='round'
+              type="primary"
+              size="large"
+              shape="round"
               icon={<PlusOutlined />}
-              onClick={() =>
-                setCreateSphereModalVisible(true)
-              }
+              onClick={() => setCreateSphereModalVisible(true)}
             />
           </Tooltip>
         </Row>
       </Sider>
     </Layout>
-  )
+  );
 }
 
-const CreateSphereModal = ({
-  visible,
-  setVisibility,
-}: any) => {
-  const [form] = Form.useForm()
-  const { setSceneObjects, sceneObjects } =
-    useSceneContext()
+const CreateSphereModal = ({ visible, setVisibility }: any) => {
+  const [form] = Form.useForm();
+  const { setSceneObjects, sceneObjects } = useSceneContext();
 
-  const handleCreateSphere = (values: {
-    [key: string]: number | string
-  }) => {
-    console.log(values)
-    const sphereData: any = {} as SphereConstructorType
-    const sphereCenter: number[] = []
+  const handleCreateSphere = (values: { [key: string]: number | string }) => {
+    const sphereData: any = {} as SphereConstructorType;
+    const sphereCenter: number[] = [];
 
     Object.keys(values).forEach((key: string) => {
-      if (key.startsWith('center'))
-        sphereCenter.push(+values[key])
-      else
-        sphereData[key as keyof typeof sphereData] =
-          values[key]
-    })
-    sphereData['center'] = sphereCenter
+      if (key.startsWith("center")) sphereCenter.push(+values[key]);
+      else sphereData[key as keyof typeof sphereData] = values[key];
+    });
+    sphereData["center"] = sphereCenter;
 
-    const sphere = new Sphere(sphereData)
+    const sphere = new Sphere(sphereData);
 
-    setSceneObjects([...sceneObjects, sphere])
-    setVisibility(false)
-    form.resetFields()
-  }
+    setSceneObjects([...sceneObjects, sphere]);
+    setVisibility(false);
+    form.resetFields();
+  };
 
   return (
     <Modal
-      title='Create New Sphere'
+      title="Create New Sphere"
       visible={visible}
       onOk={form.submit}
       onCancel={() => {
-        form.resetFields()
-        setVisibility(false)
+        form.resetFields();
+        setVisibility(false);
       }}
-      okText='Create'
-      cancelText='Cancel'
+      okText="Create"
+      cancelText="Cancel"
     >
-      <Form
-        form={form}
-        layout='vertical'
-        onFinish={handleCreateSphere}
-      >
+      <Form form={form} layout="vertical" onFinish={handleCreateSphere}>
         <Row gutter={16}>
           <Col span={8}>
             <Form.Item
-              name='radius'
-              label='Radius'
+              name="radius"
+              label="Radius"
               rules={[{ required: true }]}
+              initialValue={50}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-                min={0}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} min={0} />
             </Form.Item>
           </Col>
           <Col span={8}>
             <Form.Item
-              name='parallels'
-              label='Parallels'
+              name="parallels"
+              label="Parallels"
               rules={[{ required: true }]}
+              initialValue={8}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-                min={0}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} min={0} />
             </Form.Item>
           </Col>
           <Col span={8}>
             <Form.Item
-              name='meridians'
-              label='Meridians'
+              name="meridians"
+              label="Meridians"
               rules={[{ required: true }]}
+              initialValue={9}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-                min={0}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} min={0} />
             </Form.Item>
           </Col>
         </Row>
-        <Row gutter={16} align='middle'>
+        <Row gutter={16} align="middle">
           <Typography>Center: &nbsp;</Typography>(
           <Col span={7}>
             <Form.Item
-              name='centerX'
-              label='x'
+              name="centerX"
+              label="x"
               rules={[{ required: true }]}
+              initialValue={0}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} />
             </Form.Item>
           </Col>
           <Col span={7}>
             <Form.Item
-              name='centerY'
-              label='y'
+              name="centerY"
+              label="y"
               rules={[{ required: true }]}
+              initialValue={-50}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} />
             </Form.Item>
           </Col>
           <Col span={7}>
             <Form.Item
-              name='centerZ'
-              label='z'
+              name="centerZ"
+              label="z"
               rules={[{ required: true }]}
+              initialValue={0}
             >
-              <InputNumber
-                style={{ width: '100%' }}
-                controls={false}
-              />
+              <InputNumber style={{ width: "100%" }} controls={false} />
             </Form.Item>
           </Col>
           )
@@ -321,11 +337,9 @@ const CreateSphereModal = ({
         <Row gutter={16}>
           <Col span={12}>
             <Form.Item
-              name='name'
-              label='Sphere Name'
-              initialValue={`Sphere ${
-                sceneObjects.length + 1
-              }`}
+              name="name"
+              label="Sphere Name"
+              initialValue={`Sphere ${sceneObjects.length + 1}`}
               rules={[{ required: true }]}
             >
               <Input />
@@ -333,20 +347,18 @@ const CreateSphereModal = ({
           </Col>
           <Col span={12}>
             <Form.Item
-              name='color'
-              label='Color'
+              name="color"
+              label="Color"
               rules={[{ required: true }]}
+              initialValue="#00ff00"
             >
-              <input
-                style={{ width: '100%' }}
-                type='color'
-              />
+              <input style={{ width: "100%" }} type="color" />
             </Form.Item>
           </Col>
         </Row>
       </Form>
     </Modal>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/contexts/Scene.tsx
+++ src/contexts/Scene.tsx
@@ -1,33 +1,45 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useState,
-} from 'react'
-import { SphereType } from '../objects/Sphere'
+import { createContext, ReactNode, useContext, useState } from "react";
+import { SphereType } from "../objects/Sphere";
 
 type SceneContextType = {
-  sceneObjects: SphereType[]
-  setSceneObjects: (scene: SphereType[]) => void
-  selectedSphereId: string | null
-  setSelectedSphereId: (sphere: string) => void
-}
+  sceneObjects: SphereType[];
+  setSceneObjects: (scene: SphereType[]) => void;
+  selectedSphereId: string | null;
+  setSelectedSphereId: (sphere: string) => void;
+  translateSelectedObject: (dx: number, dy: number, dz: number) => void;
+  scaleSelectedObject: (sx: number, sy: number, sz: number) => void;
+  rotateSelectedObject: (axis: "x" | "y" | "z", angle: number) => void;
+};
 
-export const SceneContext = createContext(
-  {} as SceneContextType,
-)
+export const SceneContext = createContext({} as SceneContextType);
 
-export const SceneContextProvider = ({
-  children,
-}: {
-  children: ReactNode
-}) => {
-  const [sceneObjects, setSceneObjects] = useState<
-    SphereType[]
-  >([])
-  const [selectedSphereId, setSelectedSphereId] = useState<
-    string | null
-  >(null)
+export const SceneContextProvider = ({ children }: { children: ReactNode }) => {
+  const [sceneObjects, setSceneObjects] = useState<SphereType[]>([]);
+  const [selectedSphereId, setSelectedSphereId] = useState<string | null>(null);
+
+  const translateSelectedObject = (dx: number, dy: number, dz: number) => {
+    const selectedSphere = sceneObjects?.find(
+      (sphere: SphereType) => sphere.id === selectedSphereId
+    );
+
+    selectedSphere?.translate(dx, dy, dz);
+  };
+
+  const scaleSelectedObject = (sx: number, sy: number, sz: number) => {
+    const selectedSphere = sceneObjects?.find(
+      (sphere: SphereType) => sphere.id === selectedSphereId
+    );
+
+    selectedSphere?.scale(sx, sy, sz);
+  };
+
+  const rotateSelectedObject = (axis: "x" | "y" | "z", angle: number) => {
+    const selectedSphere = sceneObjects?.find(
+      (sphere: SphereType) => sphere.id === selectedSphereId
+    );
+
+    selectedSphere?.rotate(angle, axis);
+  };
 
   return (
     <SceneContext.Provider
@@ -36,12 +48,14 @@ export const SceneContextProvider = ({
         setSceneObjects,
         selectedSphereId,
         setSelectedSphereId,
+        translateSelectedObject,
+        scaleSelectedObject,
+        rotateSelectedObject,
       }}
     >
       {children}
     </SceneContext.Provider>
-  )
-}
+  );
+};
 
-export const useSceneContext = () =>
-  useContext(SceneContext)
+export const useSceneContext = () => useContext(SceneContext);
